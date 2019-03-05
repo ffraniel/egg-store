871bc3361088f0d7c181b0231bba6bddbcd4c0b1
ffraniel <francis.whitehead@googlemail.com>
2019-03-05 10:33:56 +0000

    added store naviation location

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      location: 'home',
+      location: 'Home',
+      storeLocation: 'Eggs',
       loading: false
     };
     this.handleNav = this.handleNav.bind(this);
+    this.handleStoreNav = this.handleStoreNav.bind(this);
   }
 
   handleNav (location) {
@@ -24,6 +26,16 @@ class App extends Component {
     }, 2000)
   }
 
+  handleStoreNav (storeLocation) {
+    console.log("store location: ", storeLocation);
+    this.setState({
+      storeLocation
+    })
+    setTimeout(()=>{
+      console.log("the store location is now set to : ", this.state.storeLocation);
+    }, 2000)
+  }
+
   render() {
     return (
       <div className="App">
@@ -31,7 +43,7 @@ class App extends Component {
           <UserNav handleNav={this.handleNav}/>
           <Logo handleNav={this.handleNav}/>
         </header>
-        <Store location={this.state.location} />
+        <Store location={this.state.location} handleStoreNav={this.handleStoreNav} />
       </div>
     );
   }
