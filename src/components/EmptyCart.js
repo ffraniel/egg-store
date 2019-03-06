d16bbae9880f3a57f910ca12670ce2954101189a
ffraniel <francis.whitehead@googlemail.com>
2019-03-06 18:39:18 +0000

    empty cart message

---
+++ src/components/EmptyCart.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './EmptyCart.css';
+
+const EmptyCart = ()=>(
+  <div className="Empty-Cart">
+    <h3>There is currently nothing in your cart</h3>
+    <h3>Keep Shopping!</h3>
+  </div>
+);
+
+export default EmptyCart;
