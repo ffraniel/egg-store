1c3437a2c97302b2ff5f37d6e202dc22f09aaba2
ffraniel <francis.whitehead@googlemail.com>
2019-03-06 02:07:19 +0000

    added store options basic component with state

---
+++ src/components/StoreOptions.js
@@ -0,0 +1,47 @@
+import React, { Component }from 'react';
+import './StoreOptions.css';
+import eggOptions from '../data/options';
+
+class StoreOptions extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedItem: {
+        number: 0,
+        includeRare: undefined
+      },
+      options: []
+    }
+  }
+
+  componentDidMount () {
+    setTimeout(()=>{
+      this.setState({
+        options: eggOptions
+      });
+    }, 450)
+
+  }
+
+  render() {
+    return (
+      <section className="Store-Options">
+        {this.state.options.map((option)=>{
+          var optionRareClass = option.includeRare ? 'Product-Option Product-Option-Rare' : 'Product-Option';
+          return(
+            <div className={optionRareClass}>
+              <h3 className="Product-Option-Title">{option.name}</h3>
+              <h5 className="Product-Option-Price">£{option.price}</h5>
+              {/* <img src={} className="" alt="eggs" /> */}
+              <p className="">{option.number} {option.includeRare ? 'Rare Breed': ''} Eggs</p>
+              <button className="Cart-Add-Button">Add to Cart</button>
+              <button classname="Buy-Now-Button">Buy Now!</button>
+            </div>
+          )
+        })}
+      </section>
+    )
+  };
+};
+
+export default StoreOptions;
