e4abc3405fdd7810d62c3d349f67eb3203282bd1
ffraniel <francis.whitehead@googlemail.com>
2019-03-06 01:58:24 +0000

    added highlighting on store nav button

--- src/components/StoreNavButton.js
+++ src/components/StoreNavButton.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './StoreNavButton.css';
 
-const StoreNavButton = (props) => (
-  <button className="store-nav-link" onClick={()=>{props.handleStoreNav(props.destination);}}>{props.destination}</button>
-);
+const StoreNavButton = (props) => {
+  var storeNavButtonClass = props.destination === props.storeLocation ? "store-nav-link highlighted-store-link" : "store-nav-link";
+  return (
+    <button className={storeNavButtonClass} onClick={()=>{props.handleStoreNav(props.destination);}}>{props.destination}</button>
+  )
+};
 
 export default StoreNavButton;
