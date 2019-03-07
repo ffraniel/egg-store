b9b1e795e2c9b4d23071a7ec5d4e775ac6cb9011
ffraniel <francis.whitehead@googlemail.com>
2019-03-07 13:40:55 +0000

    split intro from Store component

---
+++ src/components/Introduction.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Introduction.css';
+
+const Introduction = () => (
+  <div className="Introduction-container">
+      <div className="Introduction-inner">
+        <h1>Order your eggs ready for collection here</h1>
+        <p>It couldn't be easier, select how many eggs you would like, then you can arrange to pay online, or just reserve for collection.</p>
+        <div className="Introduction-border"></div>
+      </div>
+    </div>
+);
+
+export default Introduction;
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import './Store.css';
-import StoreNav from './StoreNav';
 import StoreOptions from './StoreOptions';
 
 const Store = (props)=>{
   return(
     <div className="Store-container">
       <div className="Store-inner">
-        <h1>Order your eggs ready for collection here</h1>
-        <p>It couldn't be easier</p>
-        <p>If you wish you can pay online</p>
-        <div className="Store-border"></div>
-        <h2>{props.location}</h2>
-        <StoreNav handleStoreNav={props.handleStoreNav} storeLocation={props.storeLocation} />
         <StoreOptions 
-          storeLocation={props.storeLocation} 
           addToCart={props.addToCart} 
           removeFromCart={props.removeFromCart}
+          handleNav={props.handleNav}
         />
       </div>
     </div>
