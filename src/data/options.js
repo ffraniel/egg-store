ead179be3ce8edaaea94af82e3919f6aad900f38
ffraniel <francis.whitehead@googlemail.com>
2019-03-07 13:41:42 +0000

    removed incorrect options

--- src/data/options.js
+++ src/data/options.js
@@ -1,21 +1,9 @@
 const eggOptions = [
   {
-    name: "Half a dozen eggs",
-    price: 1.2,
-    number: 6,
-    includeRare: false
-  },
-  {
     name: "Half a dozen eggs including Rare Breeds eggs",
     price: 1.8,
     number: 6,
     includeRare: true
-  },
-  {
-    name: "A dozen eggs",
-    price: 2.2,
-    number: 12,
-    includeRare: false
   },
   {
     name: "A dozen eggs including Rare Breed eggs",
