55f9e8e4ee6056780d7c4664fed3d55c8e40dd2d
ffraniel <francis.whitehead@googlemail.com>
2019-03-07 13:42:42 +0000

    improved cart markup

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -6,15 +6,16 @@ const Cart = (props) => {
   var cartVisibleClass = props.cartVisible ? 'Popup-Cart Cart-Visible' : 'Popup-Cart';
   return(
     <section className={cartVisibleClass}>
-      <h1 className="Popup-Minimizer" onClick={()=>{props.makeCartVisible()}}>-</h1>
+      <h2>Cart</h2>
+      <h1 className="Popup-Minimizer" onClick={()=>{props.makeCartVisible()}}>X</h1>
 
       {props.cartVisible && props.cart.length === 0 && 
         <EmptyCart />
       }
 
-      {props.cart.map((product)=>{
+      {props.cart.map((product, i)=>{
         return (
-          <section>
+          <section key={product.name + i}>
             <h3>{product.name}</h3>
             <h3>{product.price}</h3>
               <button onClick={()=>{props.addToCart(product, 1)}}>+</button>
