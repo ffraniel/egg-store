d64d8d7bae775e3ba9dfc8a2aaeba29ae4140756
ffraniel <francis.whitehead@googlemail.com>
2019-03-08 16:11:09 +0000

    cart layout styling, plus accumulator of price

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,6 +4,14 @@ import EmptyCart from './EmptyCart';
 
 const Cart = (props) => {
   var cartVisibleClass = props.cartVisible ? 'Popup-Cart Cart-Visible' : 'Popup-Cart';
+  // var totalCost = props.cart.reduce((acc, currentVal, currentInd, array)=>{
+  //   console.log("acc: ", acc)
+  //   return acc + (currentVal.price * currentVal.quantity).toFixed(2);
+  // }, 0);
+  var totalCost = 0;
+  props.cart.forEach((item)=>{
+    totalCost = totalCost + (item.price * item.quantity);
+  });
   return(
     <section className={cartVisibleClass}>
       <h2>Cart</h2>
@@ -12,28 +20,40 @@ const Cart = (props) => {
       {props.cartVisible && props.cart.length === 0 && 
         <EmptyCart />
       }
-
+      <div className="Cart-Grid-Titles">
+        <h3>Product</h3>
+        <h3>Price</h3>
+        <h3>Quantity</h3>
+      </div>
       {props.cart.map((product, i)=>{
         var cumulativePrice = (product.price * product.quantity).toFixed(2);
         return (
-          <section key={product.name + i}>
+          <section key={product.name + i} className="Cart-Grid">
             <h3>{product.name}</h3>
-            <h3>{product.price}</h3>
-            <button onClick={()=>{props.addToCart(product, 1)}}>+</button>
             <h3>£{cumulativePrice}</h3>
-            <button onClick={()=>{props.removeFromCart(product, 1)}}>-</button>
+            <div className="Cart-Incrementer">
+              <button onClick={()=>{props.addToCart(product, 1)}}>+</button>
+              <h3 className="Cart-Quantity-Number">{product.quantity}</h3>
+              <button onClick={()=>{props.removeFromCart(product, 1)}}>-</button>
+            </div>
           </section>
         );
       })}
       {props.cart.length > 0 &&
-        <button 
-          onClick={()=>{ 
-            props.loadingWait();
-            props.handleNav("Checkout");
-          }} 
-          className="Checkout-Button">
-            Checkout
-        </button>
+        <section className="Cart-Dashboard">
+          <div className="Cart-Dash-Grid">
+            <h3>Total </h3>
+            <h3>{totalCost.toFixed(2)}</h3>
+          </div>
+          <button 
+            onClick={()=>{ 
+              props.loadingWait();
+              props.handleNav("Checkout");
+            }} 
+            className="Checkout-Button">
+              Checkout
+          </button>
+        </section>
       }
     </section>
   )
