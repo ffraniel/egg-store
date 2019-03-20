19d61d662e6e70a23bf2bf93a3504783b883f657
ffraniel <francis.whitehead@googlemail.com>
2019-03-20 01:38:39 +0000

    altered egg options

--- src/data/options.js
+++ src/data/options.js
@@ -2,14 +2,12 @@ const eggOptions = [
   {
     name: "Half a dozen eggs including Rare Breeds eggs",
     price: 1.8,
-    number: 6,
-    includeRare: true
+    number: 6
   },
   {
     name: "A dozen eggs including Rare Breed eggs",
     price: 3.2,
-    number: 12,
-    includeRare: true
+    number: 12
   }
 ];
 
