5abdb6b7e958a80fd37e7baeb609c1f357baa63b
ffraniel <francis.whitehead@googlemail.com>
2019-03-25 17:13:02 +0000

    basic structure of reserve element

--- src/components/ReserveElement.js
+++ src/components/ReserveElement.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ReserveElement.css';
 
 const ReserveElement = () => {
+  const [ name, updateName ] = useState('');
+  const [ details, updateDetails ] = useState('');
+
+  const handleSubmitReserve = (e) => {
+    e.preventDefault();
+    console.log(`submitting details of ${name} for ${details}.`);
+    updateName('');
+    updateDetails('');
+  };
+
   return (
-    <p>Showing Reserve</p>
+    <section className="Reserve">
+      <h3>Making a reservation</h3>
+      <p>In order to reserve your order we will need some details from you</p>
+
+      <form onSubmit={handleSubmitReserve}>
+        <label>
+          Name
+          <input
+            onChange={e => updateName(e.target.value)}
+            type="text" 
+            value={name} 
+            placeholder="Name" 
+            name="name" 
+            required
+          />
+        </label>
+        <label>
+          Details
+          <textarea 
+            onChange={(e => updateDetails(e.target.value))}
+            type="textarea" 
+            value={details} 
+            placeholder="When would you be able to collect the eggs" 
+            name="name" 
+          />
+        </label>
+        <input className="CartGrid-Pay-Buttons" type="submit" value="Check Order" />
+      </form>
+    </section>
   );
 };
 
