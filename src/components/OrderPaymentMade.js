24605dcf1e1897b2954712445dc7dc84fe015dcf
ffraniel <francis.whitehead@googlemail.com>
2019-03-26 01:15:03 +0000

    added payment made order complete handling

--- src/components/OrderPaymentMade.js
+++ src/components/OrderPaymentMade.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './OrderPaymentMade.css';
+import React from "react";
+import "./OrderPaymentMade.css";
 
-const OrderPaymentMade = () => {
+const OrderPaymentMade = ({order}) => {
   return (
     <div className="Order-Completed-Payment">
-      <h3>IT WAS A PAYMENT</h3>
+      <h3>Thanks for buying our eggs, ${order.name} we appreciate it!</h3>
+      <p>We will be in contact soon about your order about when you can collect your order.</p>
     </div>
-  )
+  );
 };
 
 export default OrderPaymentMade;
