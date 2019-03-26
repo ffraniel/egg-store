5202935e398549900dfee7e6c9e62bac017c3d12
ffraniel <francis.whitehead@googlemail.com>
2019-03-26 01:01:49 +0000

    added navigation

--- src/components/OrderCompleted.js
+++ src/components/OrderCompleted.js
@@ -3,13 +3,16 @@ import './OrderCompleted.css';
 import OrderReservationMade from './OrderReservationMade';
 import OrderPaymentMade from './OrderPaymentMade';
 
-const OrderCompleted = ({order}) => {
+const OrderCompleted = ({order, handleNav}) => {
   return (
     <div className="Order-Completed">
-      <h1>Order Completed</h1>
-      <OrderReservationMade order={order} />
-      {/* {order.type === 'reservation' && <OrderReservationMade order={order} />}
-      {order.type === 'payment' && <OrderPaymentMade order={order} />} */}
+      <div className="Order-Completed-Inner">
+        <h1>Order Completed</h1>
+        <OrderReservationMade order={order} />
+        {/* {order.type === 'reservation' && <OrderReservationMade order={order} />}
+        {order.type === 'payment' && <OrderPaymentMade order={order} />} */}
+        <button className="Completed-Back-Button" onClick={()=>{handleNav('Home')}} >Back</button>
+      </div>
     </div>
   )
 };
