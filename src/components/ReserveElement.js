131f8fc91fdade14bc673d92889940f3f0ee15c9
ffraniel <francis.whitehead@googlemail.com>
2019-03-26 13:30:46 +0000

    added email and telephone forms

--- src/components/ReserveElement.js
+++ src/components/ReserveElement.js
@@ -8,6 +8,8 @@ const ReserveElement = ({
   }) => {
   const [ name, updateName ] = useState('');
   const [ details, updateDetails ] = useState('');
+  const [ email, updateEmail ] = useState('');
+  const [ telephone, updateTelephone ] = useState('');
   const [ showReserveButton, updateShowReserveButton ] = useState(false);
 
   const handleCheckReserve = (e) => {
@@ -29,7 +31,9 @@ const ReserveElement = ({
     submitOrder({
       name: name, 
       details: details,
-      paid: false
+      paid: false,
+      email: email,
+      telephone: telephone
     });
   }
 
@@ -37,9 +41,10 @@ const ReserveElement = ({
     <section className="Reserve">
       <h3>Making a reservation</h3>
       <p>In order to reserve your order we will need some details from you</p>
+      <p>* items required.</p>
 
       <form onSubmit={handleCheckReserve}>
-        <label className="reserve-label" for="name">Name</label>
+        <label className="reserve-label" for="name">Name *</label>
           <input 
             className="reserve-input reserve-text"
             onChange={e => updateName(e.target.value)}
@@ -49,6 +54,25 @@ const ReserveElement = ({
             name="name" 
             required
           />
+        <label className="reserve-label" for="email">Email</label>
+          <input 
+            className="reserve-input reserve-text"
+            onChange={e => updateEmail(e.target.value)}
+            type="text" 
+            value={email} 
+            placeholder="Email" 
+            name="email" 
+          />
+        <label className="reserve-label" for="telephone">Telephone *</label>
+          <input 
+            className="reserve-input reserve-text"
+            onChange={e => updateTelephone(e.target.value)}
+            type="text" 
+            value={telephone} 
+            placeholder="Telephone" 
+            name="telephone" 
+            required
+          />  
         <label className="reserve-label" for="details">Details</label>
           <textarea 
             className="reserve-input reserve-textarea"
@@ -57,10 +81,9 @@ const ReserveElement = ({
             value={details} 
             placeholder="Reservation details" 
             name="details" 
-            required
           />
         
-        <input className="CartGrid-Pay-Buttons" type="submit" value="Check Order" />
+        <input className="CartGrid-Pay-Buttons" type="submit" value="Check Availability" />
       </form>
 
       {showReserveButton && 
