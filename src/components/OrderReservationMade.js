99d2dd5df7f9641f30542c82fbf100b8c794d5c0
ffraniel <francis.whitehead@googlemail.com>
2019-03-26 01:01:09 +0000

    added basic useful message after completing reservation

--- src/components/OrderReservationMade.js
+++ src/components/OrderReservationMade.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './OrderReservationMade.css';
 
-const OrderReservationMade = () => {
+const OrderReservationMade = ({order}) => {
   return (
     <div className="Order-Completed-Reservation">
-      <h3>IT WAS A RESERVATION</h3>
+      <h3>Thanks for making a reservation for eggs {order.name}</h3>
+      <p>We will be in contact soon about your order</p>
     </div>
   )
 };
